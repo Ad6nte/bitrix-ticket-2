142c485dce3c9cf378f717bbdfe05f10da4fe8b4
Максим Калугин <ad6nte@gmail.com>
2024-03-31 18:01:23 +0300

    5. [ex1-13] Управление динамической информацией на сайте: отзывы. 6. [ex1-14] Раздел сайта с динамической информацией: отзывы

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -3,12 +3,9 @@
 
 <head>
 	<?$APPLICATION->ShowHead();?>
-    <title><?$APPLICATION->ShowTitle();?></title>
+    <title><?$APPLICATION->ShowTitle(false);?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8" />
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH;?>/css/reset.css" />
     <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH;?>/css/style.css" />
